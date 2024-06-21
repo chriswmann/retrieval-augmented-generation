df81492eca02d172e88c18166a3a43bd9e652f15
Chris <8133496+chriswmann@users.noreply.github.com>
2024-06-21 19:54:52 +0100

    Initial commit

--- rag_mvp/main.py
+++ rag_mvp/main.py
@@ -1,41 +1,45 @@
+import warnings
+
 import lancedb
 
-from lancedb.pydantic import LanceModel, Vector
 from lancedb.embeddings import get_registry
-from lancedb.embeddings.sentence_transformers import SentenceTransformerEmbeddings
+from lancedb.pydantic import LanceModel, Vector
 from wikipediaapi import Wikipedia
 
+MODEL_NAME: str = "BAAI/bge-m3"
+# MODEL_NAME: str = "BAAI/bge-small-en-v1.5"
 
-MODEL_NAME: str = "BAAI/bge-small-en-v1.5"
-# MODEL_NAME: str = "BAAI/bge-m3"
-
-model_registry = get_registry().get("sentence-transformers")
-model: SentenceTransformerEmbeddings = model_registry.create(
-    name=MODEL_NAME, device="cpu"
+warnings.simplefilter(action="ignore", category=FutureWarning)
+model = (
+    get_registry().get("sentence-transformers").create(name=MODEL_NAME, device="cpu")
 )
 
 
 class Document(LanceModel, arbitrary_types_allowed=True):
     text: str = model.SourceField()
-    vector: Vector(384) = model.VectorField()  # type: ignore[report-invalid-type-form]
+    vector: Vector(model.ndims()) = model.VectorField()  # type: ignore[report-invalid-type-form]
     category: str
 
 
 def main() -> None:
     wiki: Wikipedia = Wikipedia("RAGBot9000", "en")
     docs = [
-        {"text": x, "category": "cat"}
+        {"text": x[0], "category": "cat"}
         for x in [wiki.page("Maru (cat)").text.split("\n\n")]
     ]
     docs += [
-        {"text": x, "category": "painting"}
+        {"text": x[0], "category": "painting"}
         for x in [wiki.page("Venus_Anadyomene_(Titian)").text.split("\n\n")]
     ]
-    uri = "data/lancedb"
-    db = lancedb.connect(uri)
-    tbl_docs = db.create_table("documents", schema=Document, exist_ok=True)
+    uri = "./data/lancedb"
+    db: lancedb.DBConnection = lancedb.connect(uri)
+    table = db.create_table("documents", schema=Document, exist_ok=True)
+    table.add(docs)
 
-    tbl_docs.add(docs)
+    query = "Japenese cat"
+
+    actual = table.search(query).limit(1).to_pydantic(Document)[0]
+    print(actual.text)  # type: ignore[report-attribute-access-issue]
 
 
 if __name__ == "__main__":
